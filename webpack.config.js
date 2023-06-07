4f43b08f43116f7882709921d9b26b44ea28a9e0
Tatyana Sender <tatyana.sender@cogniteq.com>
2023-06-07 10:55:37 +0200

    install eslint, prettier, husky

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,41 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'index_bundle.js',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  target: 'web',
+  devServer: {
+    port: '3002',
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    target: 'web',
-    devServer: {
-        port: '3002',
-        static: {
-            directory: path.join(__dirname, 'public')
-        },
-        open: true,
-        hot: true,
-        liveReload: true,
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
     },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: 'ts-loader',
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'public', 'index.html')
-        })
-    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
 };
